75badc18f0f8107b9490a90a898b13827577b8ef
Héctor Muñoz <hectorvl@gmail.com>
2017-04-05 20:31:20 -0300

    Add feature test case

--- tests/features/ExampleTest.php
+++ tests/features/ExampleTest.php
@@ -1,19 +1,13 @@
 <?php
 
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
-
-class ExampleTest extends TestCase
+class ExampleTest extends FeatureTestCase
 {
     /**
      * A basic functional test example.
      *
      * @return void
      */
-    use DatabaseTransactions;
-
-    public function testBasicExample()
+    function test_basic_example()
     {
         $user = factory(\App\User::class)->create([
             'name'=>'Hector Munoz',
